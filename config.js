2aea9f32bf16cc1ffa1088c5cc9c443c6a853c6e
Kami Makoś <makos.kamil@o2.pl>
2018-07-13 14:46:20 +0200

    init_project

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '5899285360-hbsapcre3f265u64holrpn3rv53d86gv.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'yPJzyy6qkoHG5ra4Ml6cGD_q',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
